ca99950d57cd806aa52debe82514c250fd77555d
Karl Wang <kawang@its-pch440-39.ucsd.edu>
2019-02-22 19:17:56 -0800

    lab 1

---
+++ Tumblr/PhotoCellTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PhotoCellTableViewCell.swift
+//  Tumblr
+//
+//  Created by Karl Wang on 2/22/19.
+//  Copyright © 2019 Karl Wang. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Tumblr/PhotosViewController.swift
@@ -0,0 +1,68 @@
+//
+//  PhotosViewController.swift
+//  Tumblr
+//
+//  Created by Karl Wang on 2/22/19.
+//  Copyright © 2019 Karl Wang. All rights reserved.
+//
+
+import UIKit
+
+class PhotosViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return posts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "This is row\(indexPath.row)"
+        return cell
+    }
+    @IBOutlet weak var PhotoCell: PhotoCellTableViewCell!
+    
+    var posts:[[String: Any]] = []
+    
+    @IBOutlet weak var TableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        TableView.delegate = self
+        TableView.dataSource = self
+        // Network request snippet
+        let url = URL(string: "https://api.tumblr.com/v2/blog/humansofnewyork.tumblr.com/posts/photo?api_key=Q6vHoaVm5L1u2ZAW1fqv3Jw48gFzYVg9P0vH0VHl3GVy6quoGV")!
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        session.configuration.requestCachePolicy = .reloadIgnoringLocalCacheData
+        let task = session.dataTask(with: url) { (data, response, error) in
+            if let error = error {
+                print(error.localizedDescription)
+            } else if let data = data,
+                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
+                print(dataDictionary)
+                
+                // TODO: Get the posts and store in posts property
+                let responseDictionary = dataDictionary["response"] as! [String: Any]
+                // TODO: Reload the table view
+                self.posts = responseDictionary["posts"] as! [[String: Any]]
+            }
+        }
+        task.resume()
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
